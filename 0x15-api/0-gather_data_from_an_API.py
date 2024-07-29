1ab1d927036ea11699a7c5377602dbfb14bc3345
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-29 16:39:41 -0700

    -

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,44 +3,35 @@ import requests
 import sys
 
 
-def fetch_employee_todo_progress(employee_id):
-    # Base URL of the API
-    base_url = "https://jsonplaceholder.typicode.com/"
+def main():
+    if len(sys.argv) != 2:
+        return "Usage: python3 script.py EMPLOYEE_ID"
 
-    # Fetch user data
+    employee_id = sys.argv[1]
+    if not employee_id.isdigit():
+        return "EMPLOYEE_ID must be an integer."
+
+    employee_id = int(employee_id)
+    base_url = "https://jsonplaceholder.typicode.com/"
     user_response = requests.get(f"{base_url}users/{employee_id}")
     if user_response.status_code != 200:
-        print(f"Employee with ID {employee_id} not found.")
-        return
+        return f"Employee with ID {employee_id} not found."
 
     user_data = user_response.json()
     employee_name = user_data.get("name")
-
-    # Fetch TODO list data for the user
     todos_response = requests.get(f"{base_url}todos?userId={employee_id}")
     todos_data = todos_response.json()
-
     total_tasks = len(todos_data)
     completed_tasks = [task for task in todos_data if task.get("completed")]
     number_of_done_tasks = len(completed_tasks)
 
-    # Print the TODO list progress
-    print(f"Employee {employee_name} is done with tasks"
-          f"({number_of_done_tasks}/{total_tasks}):")
+    result = (f"Employee {employee_name} is done with tasks"
+              f"({number_of_done_tasks}/{total_tasks}):\n")
     for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+        result += f"\t {task.get('title')}\n"
+
+    return result.strip()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py EMPLOYEE_ID")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("EMPLOYEE_ID must be an integer.")
-        sys.exit(1)
-
-    fetch_employee_todo_progress(employee_id)
-
+    main()
